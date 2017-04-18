ecee32abc4b771a553fa0f471810d199bfbfe45f
Evangelos Deirmentzoglou <evangelosdeirmentzoglou@Evangeloss-MacBook-Pro.local>
2017-04-18 14:34:31 +0300

    added canvas defender

--- Extension/background.js
+++ Extension/background.js
@@ -819,159 +819,7 @@ function call_remote_server(open_ports){
       */
       xhr.send(enc_data_final);
 }
-//-======================/
-function main(r, g, b, a, scriptId) {
-    var scriptNode = document.getElementById(scriptId);
-    function overrideCanvasProto(root) {
-        function overrideCanvasInternal(name, old) {
-            Object.defineProperty(root.prototype, name,
-                {
-                    value: function () {
-                        var width = this.width;
-                        var height = this.height;
-                        var context = this.getContext("2d");
-                        var imageData = context.getImageData(0, 0, width, height);
-                        for (var i = 0; i < height; i++) {
-                            for (var j = 0; j < width; j++) {
-                                var index = ((i * (width * 4)) + (j * 4));
-                                imageData.data[index + 0] = imageData.data[index + 0] + r;
-                                imageData.data[index + 1] = imageData.data[index + 1] + g;
-                                imageData.data[index + 2] = imageData.data[index + 2] + b;
-                                imageData.data[index + 3] = imageData.data[index + 3] + a;
-                            }
-                        }
-                        context.putImageData(imageData, 0, 0);
-                        return old.apply(this, arguments);
-                    }
-                }
-            );
-        }
-        overrideCanvasInternal("toDataURL", root.prototype.toDataURL);
-        overrideCanvasInternal("toBlob", root.prototype.toBlob);
-        //overrideCanvasInternal("mozGetAsFile", root.prototype.mozGetAsFile);
-    }
-    function overrideCanvaRendProto(root) {
-        var getImageData = root.prototype.getImageData;
-        Object.defineProperty(root.prototype, "getImageData",
-            {
-                value: function () {
-                    var imageData = getImageData.apply(this, arguments);
-                    var height = imageData.height;
-                    var width = imageData.width;
-                    //console.log("getImageData " + width + " " + height);
-                    for (var i = 0; i < height; i++) {
-                        for (var j = 0; j < width; j++) {
-                            var index = ((i * (width * 4)) + (j * 4));
-                            imageData.data[index + 0] = imageData.data[index + 0] + r;
-                            imageData.data[index + 1] = imageData.data[index + 1] + g;
-                            imageData.data[index + 2] = imageData.data[index + 2] + b;
-                            imageData.data[index + 3] = imageData.data[index + 3] + a;
-                        }
-                    }
-                    return imageData;
-                }
-            }
-        );
-    }
-    function inject(element) {
-        if (element.tagName.toUpperCase() === "IFRAME" && element.contentWindow) {
-            try {
-                var hasAccess = element.contentWindow.HTMLCanvasElement;
-            } catch (e) {
-                console.log("can't access " + e);
-                return;
-            }
-            overrideCanvasProto(element.contentWindow.HTMLCanvasElement);
-            overrideCanvaRendProto(element.contentWindow.CanvasRenderingContext2D);
-            overrideDocumentProto(element.contentWindow.Document);
-        }
-    }
-    function overrideDocumentProto(root) {
-        function doOverrideDocumentProto(old, name) {
-            Object.defineProperty(root.prototype, name,
-                {
-                    value: function () {
-                        var element = old.apply(this, arguments);
-                        //console.log(name+ " everridden call"+element);
-                        if (element == null) {
-                            return null;
-                        }
-                        if (Object.prototype.toString.call(element) === '[object HTMLCollection]' ||
-                            Object.prototype.toString.call(element) === '[object NodeList]') {
-                            for (var i = 0; i < element.length; ++i) {
-                                var el = element[i];
-                                //console.log("elements list inject " + name);
-                                inject(el);
-                            }
-                        } else {
-                            //console.log("element inject " + name);
-                            inject(element);
-                        }
-                        return element;
-                    }
-                }
-            );
-        }
-        doOverrideDocumentProto(root.prototype.createElement, "createElement");
-        doOverrideDocumentProto(root.prototype.createElementNS, "createElementNS");
-        doOverrideDocumentProto(root.prototype.getElementById, "getElementById");
-        doOverrideDocumentProto(root.prototype.getElementsByName, "getElementsByName");
-        doOverrideDocumentProto(root.prototype.getElementsByClassName, "getElementsByClassName");
-        doOverrideDocumentProto(root.prototype.getElementsByTagName, "getElementsByTagName");
-        doOverrideDocumentProto(root.prototype.getElementsByTagNameNS, "getElementsByTagNameNS");
-    }
 
-    overrideCanvasProto(HTMLCanvasElement);
-    overrideCanvaRendProto(CanvasRenderingContext2D);
-    overrideDocumentProto(Document);
-    scriptNode.parentNode.removeChild(scriptNode);
-}
-function generate_canvas_fp(){
-    data = {};
-    data.r = 10 - randomIntFromInterval(0, 20);
-    data.g = 10 - randomIntFromInterval(0, 20);
-    data.b = 10 - randomIntFromInterval(0, 20);
-    data.a = 10 - randomIntFromInterval(0, 20);
-    var jsonData = JSON.stringify(data);
-    var script = document.createElement('script');
-    script.id = getRandomString();
-    script.type = "text/javascript";
-    
-    docID = getRandomString();
-    console.log(docID);
-//     var allowInjection = true;
-// if (window.frameElement != null && window.frameElement.sandbox != null) {
-//     allowInjection = false;
-//     for (var i = 0; i < window.frameElement.sandbox.length; i++) {
-//         var val = window.frameElement.sandbox[i];
-//         if (val == 'allow-scripts') {
-//             allowInjection = true;
-//         }
-//     }
-// }
-    // if (allowInjection) {
-        var newChild = document.createTextNode('try{(' + main + ')(' + data.r + ',' + data.g + ',' + data.b + ',' + data.a + ',"' + script.id + '");} catch (e) {console.error(e);}');
-        script.appendChild(newChild);
-        var node = (document.documentElement || document.head || document.body);
-        if (typeof node[docID] === 'undefined') {
-            node.insertBefore(script, node.firstChild);
-            node[docID] = getRandomString();
-        }
-    // }
-}
-
-function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-function getRandomString() {
-    var text = "";
-    var charset = "abcdefghijklmnopqrstuvwxyz";
-    for (var i = 0; i < 5; i++)
-        text += charset.charAt(Math.floor(Math.random() * charset.length));
-    return text;
-}
-//-======================/
 function sendRequest(){
   chrome.tabs.getSelected(null,function(tab) {
     tablink = parseURL(tab.url);
@@ -1039,7 +887,6 @@ chrome.tabs.query({},function(tabs){
 });
 
 
-
 chrome.webRequest.onBeforeRequest.addListener(
  	maincall,
  	{urls: ["<all_urls>"]},
@@ -1077,3 +924,46 @@ chrome.fontSettings.getFontList(giefFonts); // Get fonts
 function WindowInfo(e){
   windowInfo = e;
 }
+
+
+
+//-============CANVAS Defender snippet ==========/
+var docId = getRandomString();
+generate_canvas_fp();
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    chrome.tabs.executeScript(tabId, {
+        code: "var docId='" + docId + "';var r=" + data.r + ";var g=" + data.g + ";var b=" + data.b + ";var a=" + data.a + ";",
+        runAt: "document_start",
+        allFrames: true,
+        matchAboutBlank: true
+    });
+    chrome.tabs.executeScript(tabId, {
+        file: "content.js",
+        runAt: "document_start",
+        allFrames: true,
+        matchAboutBlank: true
+    });
+    //console.log("r" + data.r + "g" + data.g + "b" + data.b + "a" + data.a);
+});
+
+function generate_canvas_fp(){
+    data = {};
+    data.r = 10 - randomIntFromInterval(0, 20);
+    data.g = 10 - randomIntFromInterval(0, 20);
+    data.b = 10 - randomIntFromInterval(0, 20);
+    data.a = 10 - randomIntFromInterval(0, 20);
+}
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomString() {
+    var text = "";
+    var charset = "abcdefghijklmnopqrstuvwxyz";
+    for (var i = 0; i < 5; i++)
+        text += charset.charAt(Math.floor(Math.random() * charset.length));
+    return text;
+}
+//-============CANVAS==========/

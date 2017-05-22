89e05f0824fc9b9c105b5c6987d4262eaac05f9c
Evangelos Deirmentzoglou <evangelosdeirmentzoglou@Evangeloss-MacBook-Pro.local>
2017-05-22 14:03:17 +0300

    updated server IP

--- Extension/background.js
+++ Extension/background.js
@@ -20,7 +20,7 @@ var appVersion = navigator.appVersion;
 var plugins = [];
 var start;
 var ips = [];
-var URL = "https://192.168.149.128:3001";
+var URL = "https://83.212.171.15:3001";
 var   enc_plugins = [],
       enc_userAgent = "",
       enc_timezone = "",
